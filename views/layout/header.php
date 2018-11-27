9667c38bc19ea6ae1a35d15c052a655647c4f85e
layomlg <dpelayo@mlg.com.mx>
2018-11-27 16:47:00 -0600

    archivo linkeado

---
+++ views/layout/header.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-expand fixed-top">
+  <a class="navbar-brand" href="#"><img src="img/svg/logo.svg"></a>
+  <div class="navbar-collapse">
+    <div class="mt-2 mt-md-0 ml-auto">
+      <img src="img/svg/alert.svg">
+      <img src="img/svg/profile.svg">
+    </div>
+  </div>
+</nav>
---
+++ views/layout/sidebar.php
@@ -0,0 +1,16 @@
+<div class="sidebar">
+  <ul class="sidebar-nav">
+    <li>
+      <a href="#">Prospectos</a>
+    </li>
+    <li>
+      <a href="#">Estadística</a>
+    </li>
+    <li>
+      <a href="#">Información MLG</a>
+    </li>
+    <li>
+      <a href="#">Calendario</a>
+    </li>
+  </ul>
+</div>
